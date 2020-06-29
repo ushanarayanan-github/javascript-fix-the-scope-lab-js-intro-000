26a64458810a318775157e8aea18ae8fc8b73fad
creative-buffer-9578 <cmuusha@gmail.com>
2020-06-29 03:30:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ var funkyFunction = function() {
     return "FUNKY!"
   }
 }
+funkyFunction();
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
